40205001b363f6a4ea527dab04066b50b1c1f9f2
Mingdong Luo <mingdong.luo@sap.com>
2014-12-07 02:03:09 +0800

    Add main page

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -1,4 +1,28 @@
 class TodosController < ApplicationController
   def index
   end
+
+  def all
+     @todos = Todo.all
+     render json: @todos
+  end
+
+  def show
+      @todo = Todo.find(params[:id])
+      render json: @todo
+  end
+
+  def new
+      @todo = Todo.new(params[:todo])
+      @todo.save
+      render json: @todo
+  end
+
+  def update
+  end
+
+  def delete
+      render json: params[:id]
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   root 'todos#index'
+  get 'todos/all' => 'todos#all'
+  get 'todos/delete/:id' => 'todos#delete'
   resources :todos
 
   # The priority is based upon order of creation: first created -> highest priority.
