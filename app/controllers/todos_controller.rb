3e0a8515c68b71c853a5454148f95508a2089e21
Mingdong Luo <mdluo@nsmss.com>
2015-01-18 20:42:57 +0800

    Update indentation

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -3,48 +3,48 @@ class TodosController < ApplicationController
   end
 
   def all
-     @todos = Todo.all
-     render json: @todos 
+    @todos = Todo.all
+    render json: @todos
   end
 
   def create
-      @todo = Todo.new
-	  @todo.task = params[:task]
-	  @todo.completed = params[:completed]
-      @todo.save
-      render json: @todo
+    @todo = Todo.new
+    @todo.task = params[:task]
+    @todo.completed = params[:completed]
+    @todo.save
+    render json: @todo
   end
 
   def update
-	  @todo = Todo.find(params[:id])
-	  @todo.completed = !@todo.completed
-      @todo.save
-      render json: @todo
+    @todo = Todo.find(params[:id])
+    @todo.completed = !@todo.completed
+    @todo.save
+    render json: @todo
   end
 
   def toggle
-	  @todos = Todo.all
-	  @todos.find_each() do |todo|
-		  todo.completed = params[:completed]
-		  todo.save
-	  end
+    @todos = Todo.all
+    @todos.find_each() do |todo|
+    todo.completed = params[:completed]
+    todo.save
+    end
       render json: @todos
   end
 
   def delete
-	  if params[:id] != "-1"
-		  @todo = Todo.find(params[:id])
-		  @todo.destroy
-		  render json: @todo
-	  else
-		  @todos = Todo.all
-		  @todos.find_each() do |todo|
-			  if todo.completed
-			  	todo.destroy
-			  end
-		  end
-		  render json: @todos
-	  end
+    if params[:id] != "-1"
+      @todo = Todo.find(params[:id])
+      @todo.destroy
+      render json: @todo
+    else
+      @todos = Todo.all
+      @todos.find_each() do |todo|
+        if todo.completed
+          todo.destroy
+        end
+      end
+      render json: @todos
+    end
   end
 
 end
