1c083797d338c7acab1d158d0e40ca3cdace2c4b
Mingdong Luo <mdluo@nsmss.com>
2015-01-30 05:17:57 +0800

    Update todos match user by session

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -1,12 +1,23 @@
 class TodosController < ApplicationController
+
+  private
+  def get_user
+    if session[:user_id] == nil
+      if Todo.maximum("user_id")
+        session[:user_id] = Todo.maximum("user_id").to_i + 1
+      else
+        session[:user_id] = 1
+      end
+    end
+  end
+
+  public
   def index
+    get_user
   end
 
   def all
-    @todos = Array.new
-    if cookies[:lat_lon] != nil
-        cookies[:lat_lon].split(',').each { |value| @todos.push(Todo.where(:id => value.to_i).first) }
-    end
+    @todos = Todo.where(user_id: session[:user_id])
     render json: @todos
   end
 
@@ -14,68 +25,54 @@ class TodosController < ApplicationController
     @todo = Todo.new
       @todo.task = params[:task]
       @todo.completed = params[:completed]
+      @todo.user_id = session[:user_id]
     @todo.save
-    if cookies[:lat_lon] != nil
-        @idSet = cookies[:lat_lon].split(',')
-    else
-        @idSet = Array.new
-    end
-    @idSet.push @todo.id
-    cookies.permanent[:lat_lon] = @idSet.join(',')
     render json: @todo
   end
 
   def update
     @todo = Todo.find(params[:id])
+    if @todo.user_id == session[:user_id]
       @todo.completed = !@todo.completed
       @todo.save
-    render json: @todo
+      render json: @todo
+    else
+      render []
+    end
   end
 
   def toggle
     @todos = Array.new
-    if cookies[:lat_lon] != nil
-      cookies[:lat_lon].split(',').each do |t|
-        tmpTodo = Todo.find(t)
-        tmpTodo.completed = params[:completed]
-        tmpTodo.save
-        @todos.push(tmpTodo)
+    Todo.find_each do |todo|
+      if todo.user_id == session[:user_id]
+        todo.completed = params[:completed]
+        @todos.push(todo)
+        todo.save
       end
     end
     render json: @todos
   end
 
   def delete
-    @idSet = cookies[:lat_lon].split(',')
-    if params[:id] != "-1"
-      @todo = Todo.find(params[:id])
-      @idSet = @idSet - [@todo.id.to_s]
-      cookies.permanent[:lat_lon] = @idSet.join(',')
+    @todo = Todo.find(params[:id])
+    if @todo.user_id == session[:user_id]
       @todo.destroy
       render json: @todo
     else
-      cookies.permanent[:lat_lon] = ""
-      @todos = Todo.all
-      @todos.find_each() do |todo|
-        if todo.completed
-          todo.destroy
-        end
-      end
-      render json: @todo
+      render []
     end
   end
 
   def qrcode
-    if cookies[:lat_lon] != nil
-      content = Array.new
-      cookies[:lat_lon].split(',').each do |id|
-        task = Todo.where(:id => id.to_i).first
-        content.push :t => task[:task], :c => task[:completed]
-      end
-      data = { succeeded: content.size > 0, code: view_context.generate_qrcode_base64(content.to_json, 384) }
-    else
-      data = { succeeded: false }
+    content = Array.new
+    @todos = Todo.where(user_id: session[:user_id])
+    @todos.each do |todo|
+      content.push :t => todo[:task], :c => todo[:completed]
     end
+    data = {
+      succeeded: content.size > 0,
+      code: view_context.generate_qrcode_base64( content.to_json, 384 )
+    }
     render json: data
   end
 end
