22591090ed250fc84620c71b9f6345b2907cb1d6
Mingdong Luo <mdluo@nsmss.com>
2015-01-30 04:13:05 +0800

    Add user ref

--- db/schema.rb
+++ db/schema.rb
@@ -11,14 +11,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150127124207) do
+ActiveRecord::Schema.define(version: 20150128142817) do
 
   create_table "todos", force: true do |t|
     t.string   "task"
     t.boolean  "completed"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer  "user_id"
   end
+
+  add_index "todos", ["user_id"], name: "index_todos_on_user_id"
 
   create_table "users", force: true do |t|
     t.string   "email",            null: false
