19ddddaa775b58f9a2013a7d67f19569e0c5ad4d
Mingdong Luo <mdluo@nsmss.com>
2015-02-15 13:37:36 +0800

    Remove user resource

--- db/schema.rb
+++ db/schema.rb
@@ -11,26 +11,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150128142817) do
+ActiveRecord::Schema.define(version: 20150128133903) do
 
-  create_table "todos", force: true do |t|
-    t.string   "task"
+  create_table "todos", force: :cascade do |t|
+    t.string   "task",       limit: 255
     t.boolean  "completed"
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.integer  "user_id"
   end
-
-  add_index "todos", ["user_id"], name: "index_todos_on_user_id"
-
-  create_table "users", force: true do |t|
-    t.string   "email",            null: false
-    t.string   "crypted_password"
-    t.string   "salt"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-  end
-
-  add_index "users", ["email"], name: "index_users_on_email", unique: true
 
 end
